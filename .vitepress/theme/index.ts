2f4ef09b8430dc8ee2719115e7ab838c086a1ea2
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-02-02 11:43:56 +0100

    Refactor theme index file for improved code consistency and formatting

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,15 @@
-import type { Theme } from 'vitepress'
-import 'vitepress-plugin-sandpack/dist/style.css'
-import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
-import DefaultTheme from 'vitepress/theme'
-import Sandbox from './components/Sandbox.vue'
-import './styles/vars.css'
+import type { Theme } from "vitepress";
+import "vitepress-plugin-sandpack/dist/style.css";
+import { enhanceAppWithTabs } from "vitepress-plugin-tabs/client";
+import DefaultTheme from "vitepress/theme";
+import Sandbox from "./components/Sandbox.vue";
+import "./styles/vars.css";
 
 export default {
-  extends: DefaultTheme,
-  enhanceApp(ctx) {
-    DefaultTheme.enhanceApp(ctx);
-    ctx.app.component('Sandbox', Sandbox);
-    enhanceAppWithTabs(ctx.app)
-
-  },
-} satisfies Theme
+    extends: DefaultTheme,
+    enhanceApp(ctx) {
+        DefaultTheme.enhanceApp(ctx);
+        ctx.app.component("Sandbox", Sandbox);
+        enhanceAppWithTabs(ctx.app);
+    },
+} satisfies Theme;
